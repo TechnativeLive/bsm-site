153542c77a0ec0111bd2425df83517d80f385630
Ross Cooper <37559715+Froskk@users.noreply.github.com>
2023-05-09 14:39:28 +0100

    Initial config

---
+++ tailwind.config.ts
@@ -0,0 +1,54 @@
+import { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+        display: ['var(--font-display)'],
+        numeric: ['var(--font-numeric)'],
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#002550',
+          50: '#EEFEFF',
+          100: '#D7FBFF',
+          200: '#A9F1FF',
+          300: '#7BE3FF',
+          400: '#4DD0FF',
+          500: '#20B9FF',
+          600: '#0099F1',
+          700: '#0072C3',
+          800: '#005095',
+          900: '#003267',
+          950: '#002550',
+        },
+        secondary: {
+          DEFAULT: '#FFED00',
+          50: '#FDFFCF',
+          100: '#FDFFB8',
+          200: '#FFFF8A',
+          300: '#FFFB5C',
+          400: '#FFF52E',
+          500: '#FFED00',
+          600: '#D6C200',
+          700: '#AD9900',
+          800: '#857200',
+          900: '#5C4D00',
+          950: '#473B00',
+        },
+        neutral: '#9B9B9B',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
