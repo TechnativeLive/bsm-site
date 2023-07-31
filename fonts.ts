e9d7897eedfb16a550c0e6c7121d3eea6d933b2a
Ross Cooper <37559715+Froskk@users.noreply.github.com>
2023-07-31 08:52:08 +0100

    restore next/fonts imports for opengraph images

--- fonts.ts
+++ fonts.ts
@@ -1,17 +1,17 @@
-// import { Audiowide, Figtree, Saira } from 'next/font/google';
+import { Audiowide, Figtree, Saira } from 'next/font/google';
 
 // TODO: Optimise loaded font subsets etc.
 // https://nextjs.org/docs/app/building-your-application/optimizing/fonts
 // https://tailwindcss.com/blog/tailwindcss-v3-3#configure-font-variation-settings-for-custom-font-families
-// export const figtree = Figtree({ variable: '--font-sans', subsets: ['latin'], display: 'swap' });
+export const figtree = Figtree({ variable: '--font-sans', subsets: ['latin'], display: 'swap' });
 
-// export const audiowide = Audiowide({
-//   variable: '--font-display',
-//   weight: '400',
-//   subsets: ['latin'],
-//   display: 'block',
-// });
+export const audiowide = Audiowide({
+  variable: '--font-display',
+  weight: '400',
+  subsets: ['latin'],
+  display: 'block',
+});
 
-// export const saira = Saira({ variable: '--font-numeric', subsets: ['latin'], display: 'swap' });
+export const saira = Saira({ variable: '--font-numeric', subsets: ['latin'], display: 'swap' });
 
-// console.log({ figtree, audiowide, saira });
+console.log({ figtree, audiowide, saira });
