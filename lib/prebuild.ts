460d0129b99628027e71a1bd3c81678f728ef865
Ross Cooper <37559715+Froskk@users.noreply.github.com>
2024-05-29 15:33:31 +0100

    update cache control on rider data

--- lib/prebuild.ts
+++ lib/prebuild.ts
@@ -12,11 +12,32 @@ import { put } from '@vercel/blob';
     });
     const riders = await fetch(ridersQuery).then((res) => res.json());
 
-    const riderData = riders.data.map((rider: any) => ({
-      ...rider,
-      id: `_${rider.bib.toUpperCase()}_${rider.lastname.toUpperCase()}`,
-      title: `${rider.firstname} ${rider.lastname}, ${rider.bib}`,
-    }));
+    const riderData = riders.data
+      .map((rider: any) => ({
+        ...rider,
+        id: `_${rider.bib.toUpperCase()}_${rider.lastname.toUpperCase()}`,
+        title: `${rider.bib} - ${rider.lastname}, ${rider.firstname}`,
+      }))
+      .sort((a: { bib: string }, b: { bib: string }) => {
+        const aNum = Number(a.bib);
+        const bNum = Number(b.bib);
+        const aIsNaN = Number.isNaN(aNum);
+        const bIsNaN = Number.isNaN(bNum);
+
+        if (aIsNaN && bIsNaN) {
+          return a.bib.localeCompare(b.bib);
+        }
+
+        if (aIsNaN && !bIsNaN) {
+          return 1;
+        }
+
+        if (!aIsNaN && bIsNaN) {
+          return -1;
+        }
+
+        return aNum - bNum;
+      });
 
     // const riderDataDirectory = path.join(process.cwd(), 'public', 'data', 'riders.json');
     // await writeFile(riderDataDirectory, JSON.stringify(riderData), {
@@ -28,6 +49,7 @@ import { put } from '@vercel/blob';
       access: 'public',
       contentType: 'application/json',
       addRandomSuffix: false,
+      cacheControlMaxAge: 0,
     });
     console.log(`Rider data written to ${url}`);
   } catch (e) {
