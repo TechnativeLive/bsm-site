d2716f1ac09ca7663050dc6ada518b72cf91172f
Ross Cooper <rossadamcooper@gmail.com>
2025-08-20 11:56:35 +0100

    force redeploy while strapi vercel broken

--- app/error.tsx
+++ app/error.tsx
@@ -3,5 +3,6 @@
 import { ErrorComponent } from '@/components/error';
 
 export default function Error({ error, reset }: { error: Error; reset: () => void }) {
-  return <ErrorComponent error={error} reset={reset} />;
+  return <ErrorComponent error={error} reset={reset} />
+  ;
 }
